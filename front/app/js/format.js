4663e0fa68a77238a2e0e4f386c8b3f8ca8ecd9b
wxn0brP <toja.filiper@gmail.com>
2024-08-03 12:25:39 +0200

    feat: preview media

--- front/app/js/format.js
+++ front/app/js/format.js
@@ -1,8 +1,13 @@
 const format = {
-    formatMess(mess, div){
-        mess = format.changeText(mess);
-        
+    formatMess(mess_plain, div){
+        let mess = format.changeText(mess_plain);
         div.innerHTML = mess;
+
+        const elemensts = format.getElements(mess_plain);
+        for(const element of elemensts){
+            div.appendChild(document.createElement('br'));
+            div.appendChild(element);
+        }
     },
 
     changeText(text){
@@ -58,5 +63,147 @@ const format = {
             setTimeout(() => mess.classList.remove("res_msg__animate"), 3000); 
         });
         div.addUp(resMsgDiv);
+    },
+
+    getElements(text){
+        const regex = /(https?:\/\/[^\s]+)/g;
+        const matches = text.match(regex);
+        if(!matches) return [];
+        return matches.map(link => format.mediaPreview(link)).filter(ele => !!ele);
+    },
+
+    mediaPreview(link){
+        if(!link) return;
+
+        function chcek(link){
+            const xhr = new XMLHttpRequest();
+            xhr.open("HEAD", link, false);
+            xhr.send();
+            return xhr.status == 200;
+        }
+
+        if(/\.(mp3|wav|ogg|.m4a)$/i.test(link)){
+            if(!chcek(link)) return;
+            const ele = document.createElement('audio');
+            ele.src = link;
+            ele.controls = true;
+            return ele;
+        }
+
+        if(/\.(mp4|mkv|webm|avi)$/i.test(link)){
+            if(!chcek(link)) return;
+            const ele = document.createElement('video');
+            ele.src = link;
+            ele.controls = true;
+            ele.style.maxWidth = '65%';
+            ele.style.height = 'auto'; 
+            ele.style.borderRadius = '2rem';
+            return ele;
+        }
+
+        if(/\.(png|jpg|gif|ico|jpeg|webp)$/i.test(link)){
+            if(!chcek(link)) return;
+            const ele = document.createElement('img');
+            ele.src = link;
+            ele.style.maxWidth = '100%';
+            ele.style.height = 'auto';
+            return ele;
+        }
+
+        if(link.includes("youtube.com") || link.includes("youtu.be")) {
+            function extractYouTubeVideoId(link) {
+                const match = link.match(/(?:\?v=|\/embed\/|\.be\/|\/v\/|\/\d{1,2}\/|\/e\/|watch\?v=|youtu\.be\/|youtube\.com\/(?:v|e|embed)\/|youtube\.com\/user\/[^#\/]+#p\/[^#\/]+\/)([^"&?\/ ]{11})/);
+                return (match && match[1]) ? match[1] : null;
+            }
+            
+            const videoId = extractYouTubeVideoId(link);
+            const iframe = document.createElement('iframe');
+            iframe.src = `https://www.youtube.com/embed/${videoId}`;
+            
+            iframe.allowFullscreen = true;
+            iframe.style.maxWidth = '100%';
+            iframe.style.width = '500px';
+            iframe.style.height = '300px';
+            iframe.style.borderRadius = '2rem';
+            return iframe;
+        }
+        
+        if(link.includes("tiktok.com")){
+            const iframe = document.createElement('iframe');
+            function extractTikTokVideoId(link){
+                const regex = /tiktok\.com\/(?:@[\w.-]+\/video\/|v\/|embed\/v2\/)([\w-]+)/;
+                const match = link.match(regex);
+                return match ? match[1] : null;
+            }
+            const videoId = extractTikTokVideoId(link);
+            iframe.src = `https://www.tiktok.com/embed/v2/${videoId}`;
+            iframe.allowfullscreen = true;
+            
+            iframe.style.maxWidth = '100%';
+            iframe.style.width = '400px';
+            iframe.style.height = '700px';
+            iframe.style.borderRadius = '2rem';
+            return iframe;
+        }
+
+        if(link.includes("reddit.com")){
+            const l = link.split("?") || [link];
+            link = l[0];
+            if(!link.endsWith("/")) link += "/";
+            
+            const api = JSON.parse(cw.get(`${link}.json?limit=2`));
+            const post = api[0]?.data.children[0]?.data;
+            const ele = document.createElement("div");
+    
+            const title = post.title;
+            const author = post.author;
+            ele.innerHTML = `autor: ${author}<br />tytuł: ${title}`
+            return ele;
+        }
+
+        if(link.includes("spotify.com")){
+            const iframe = document.createElement('iframe');
+            function extractSpotifyId(link){
+                const trackMatch = link.match(/track\/([a-zA-Z0-9]+)/);
+                if(trackMatch && trackMatch[1]){
+                    return "track/"+trackMatch[1];
+                }
+                const playlistMatch = link.match(/playlist\/([a-zA-Z0-9]+)/);
+                if(playlistMatch && playlistMatch[1]){
+                    return "playlist/"+playlistMatch[1];
+                }
+                return null;
+            }
+            const videoId = extractSpotifyId(link);
+            if(!videoId) return null;
+            iframe.src = `https://open.spotify.com/embed/${videoId}`;
+            iframe.allowfullscreen = true;
+            
+            iframe.style.maxWidth = '100%';
+            iframe.style.width = '400px';
+            iframe.style.height = '84px';
+            iframe.style.borderRadius = '1rem';
+            return iframe;
+        }
+
+        const extRegex = /\.(js|html|css|py|c|cpp|java|cs|php|rb|swift|kt|ts|go|rs|pl|sh|sql|r|m|txt)$/i
+        if(extRegex.test(link)){
+            const language = link.match(extRegex)[0].replace(".","");
+    
+            const pre = document.createElement("pre");
+            pre.classList.add("preCode");
+            pre.innerHTML = `<span style="color: green;">${language}</span>:<br /><br />`;
+            pre.style.overflow = "auto";
+            
+            const code = document.createElement("code");
+            const file = cw.get(link);
+            code.innerHTML = hljs.highlight(file, {language}).value;
+            if(file.length>500 || file.split("\n").length > 10){
+                pre.style.maxHeight = "17rem";
+            }
+    
+            pre.appendChild(code);
+            return pre;
+        }
     }
 }
