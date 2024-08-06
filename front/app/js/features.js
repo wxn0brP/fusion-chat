674e6a5fa10ed66f580c5fa5c1bfcafaa7a4b8af
wxn0brP <toja.filiper@gmail.com>
2024-08-06 17:48:29 +0200

    refactor: organizate mess func

--- front/app/js/features.js
+++ front/app/js/features.js
@@ -22,3 +22,69 @@ setupSwipe(
         // down
     }
 );
+
+(function initEmocji(){
+    const emoticonMenu = document.querySelector("#emocjiDiv_container");
+
+    const emotkiUnicode = [
+        [128512, 128591],
+        // [127744, 127884],
+        // [128640, 128704],
+        // [127462, 127487],
+        // [9728, 9983],
+        // [9984, 10175]
+    ];
+
+    function emit(emoticon){
+        const event = new CustomEvent('emocji', {
+            detail: emoticon,
+        });
+        emocjiDiv.dispatchEvent(event);
+    }
+
+    emotkiUnicode.forEach(range => {
+        for(let i = range[0]; i <= range[1]; i++){
+            const emoticon = String.fromCodePoint(i);
+            const div = document.createElement('div');
+            div.textContent = emoticon;
+            div.className = 'emocji';
+            div.onclick = () => emit(emoticon);
+            emoticonMenu.appendChild(div);
+        }
+    });
+})();
+
+messInput.addEventListener("paste", function(e){
+    const items = (e.clipboardData || e.originalEvent.clipboardData).items;
+    
+    for(const item of items){
+        if(item.type.indexOf("image") === -1) continue;
+        e.preventDefault();
+        messFunc.sendFile(item.getAsFile())
+    };
+});
+
+(function initDragAndDrop(){
+    const app = document.querySelector("#app");
+    app.addEventListener("dragover", function(e){
+        e.preventDefault();
+        e.stopPropagation();
+    });
+
+    app.addEventListener("dragenter", function(e){
+        e.preventDefault();
+        e.stopPropagation();
+    });
+
+    app.addEventListener("drop", function(e){
+        e.preventDefault();
+        e.stopPropagation();
+
+        if(vars.chat.to == "main") return;
+
+        const files = e.dataTransfer.files;
+        for(const file of files){
+            messFunc.sendFile(file);
+        }
+    });
+})();
---
+++ front/app/js/messSocket.js
@@ -0,0 +1,126 @@
+messInput.addEventListener("keydown", (e) => {
+    //if shift + enter - new line
+    if(e.key == "Enter" && e.shiftKey) return;
+
+    if(e.key == "Enter"){
+        e.preventDefault();
+        messFunc.sendMess();
+    }
+});
+
+messInput.addEventListener("input", messStyle.sendBtnStyle);
+
+socket.on("mess", (data) => {
+    const tom = data.toM.replace("$", "");
+
+    if(!vars.lastMess[tom]) vars.lastMess[tom] = {};
+    if(!vars.lastMess[tom][vars.chat.chnl]) vars.lastMess[tom][vars.chat.chnl] = { read: null, mess: null };
+    vars.lastMess[tom][vars.chat.chnl].mess = data._id;
+
+    if(data.to != "@"){
+        if(vars.chat.to !== data.to){
+            if(data.to.startsWith("$")){
+                uiFunc.uiMsg("Recive message from "+apis.www.changeUserID(data.fr));
+            }
+        }
+        if(vars.chat.to !== data.to || vars.chat.chnl !== data.chnl){
+            renderFunc.privs();
+            return;
+        }
+    }
+
+    messFunc.addMess(data);
+    vars.temp.makeIsRead = data._id;
+    setTimeout(() => {
+        if(vars.temp.makeIsRead != data._id) return;
+        vars.temp.makeIsRead = null;
+        socket.emit("markAsRead", vars.chat.to, vars.chat.chnl, data._id);
+    }, 1000);
+    
+    vars.lastMess[tom][vars.chat.chnl].read = data._id;
+    renderFunc.privs();
+    messStyle.hideFromMessageInfo();
+    messStyle.colorRole();
+});
+
+socket.on("getMess", (data) => {
+    try{
+        data.forEach((mess) => {
+            try{
+                messFunc.addMess({
+                    fr: mess.fr,
+                    msg: mess.msg,
+                    _id: mess._id,
+                    e: mess.lastEdit ? mess.lastEdit : false,
+                    res: mess.res,
+                    reacts: mess.reacts || {},
+                }, false, true);
+            }catch(e){
+                lo(e);
+                lo(mess);
+                const div = document.createElement("div");
+                div.innerHTML = `<span style="color: red;">Failed to load this message!</span>`;
+                messagesDiv.add(div);
+            }
+        });
+        messStyle.hideFromMessageInfo();
+        setTimeout(coreFunc.socrollToBottom, 30);
+    }catch(e){
+        lo(e);
+        const div = document.createElement("div");
+        div.innerHTML = `<span style="color: red;">"Failed to load the message! :("</span>`;
+        messagesDiv.add(div);
+    }
+    messStyle.colorRole();
+});
+
+socket.on("deleteMess", (id) => {
+    document.querySelector("#mess__"+id)?.remove();
+    messStyle.hideFromMessageInfo();
+});
+
+socket.on("editMess", (id, msg, time) => {
+    const messageDiv = document.querySelector("#mess__"+id+" .mess_content");
+    if(!messageDiv) return;
+    messageDiv.setAttribute("_plain", msg);
+    format.formatMess(msg, messageDiv);
+    messageDiv.innerHTML += editMessText.replace("$$", coreFunc.formatDateFormUnux(parseInt(time, 36)));
+
+    const responeMessages = document.querySelectorAll(`[resMsgID=${id}] .res_msg`);
+    responeMessages.forEach(mess => {
+        mess.innerHTML = msg;
+    });
+    messStyle.hideFromMessageInfo();
+});
+
+socket.on("reactToMess", (uid, server, messId, react) => {
+    if(vars.chat.to.replace("$","") != server) return;
+    
+    const mess = document.querySelector("#mess__"+messId);
+    if(!mess) return;
+
+    const reactSpan = mess.querySelector(`span[_key="${react}"]`);
+    if(!reactSpan){
+        const span = document.createElement("span");
+        span.setAttribute("_key", react);
+        span.setAttribute("_users", uid);
+        span.innerHTML = react + " 1";
+        span.title = apis.www.changeUserID(uid);
+        span.addEventListener("click", () => {
+            socket.emit("reactToMess", server, messId, react);
+        });
+        mess.querySelector(".mess_reacts").appendChild(span);
+        messStyle.styleMessReacts(mess.querySelector(".mess_reacts"));
+        return;
+    }
+
+    let users = reactSpan.getAttribute("_users").split(",");
+    if(users.includes(uid)){
+        users = users.filter(u => u != uid);
+    }else{
+        users.push(uid);
+    }
+
+    reactSpan.setAttribute("_users", users.join(","));
+    messStyle.styleMessReacts(mess.querySelector(".mess_reacts"));
+});
