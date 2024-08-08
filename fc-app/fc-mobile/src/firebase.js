881e1416dbddf1a495f971c2b9d20bebd285fd9b
wxn0brP <toja.filiper@gmail.com>
2024-08-08 19:17:40 +0200

    Refactor | mobile app

---
+++ fc-app/fc-mobile/src/firebase.js
@@ -0,0 +1,21 @@
+import messaging from '@react-native-firebase/messaging';
+import axios from "axios";
+import config from "./config";
+
+const registerApp = async (id, user) => {
+    try{
+        const token = await messaging().getToken();
+        const res = await axios.post(config.link+"/notif-reg", {
+            token,
+            id,
+            user
+        });
+        console.log("res token add", res.data.err, res.data.msg);
+    }catch(e){
+        console.error(e.message);
+    }
+}
+
+export default {
+    registerApp
+}
