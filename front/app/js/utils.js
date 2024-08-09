1ded8b28f8ab849a8e963b6cd0bc1f05b08ead80
wxn0brP <toja.filiper@gmail.com>
2024-08-10 00:49:50 +0200

    Fix | translations

--- front/app/js/utils.js
+++ front/app/js/utils.js
@@ -2,4 +2,9 @@ const utils = {
     ss(){
         return window.innerWidth < 800;
     },
+
+    parseJSONC(jsonc){
+        const json = jsonc.replace(/\/\/.*|\/\*[\s\S]*?\*\//g, '');
+        return JSON.parse(json);
+    }
 }
