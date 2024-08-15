bd07f0a68dc1b985c49d9898e5b4f6497206b340
wxn0brP <toja.filiper@gmail.com>
2024-08-15 02:57:56 +0200

    fix: mobile app

--- fc-app/fc-mobile/src/config.js
+++ fc-app/fc-mobile/src/config.js
@@ -1,6 +1,6 @@
 import { version } from "../package.json";
 
 export default {
-    link: __DEV__ ? "http://192.168.0.15:1478" : "https://fusion.ct8.pl",
+    link: /*__DEV__ ? "http://192.168.0.15:1478" : */"https://fusion.ct8.pl",
     version,
 }
