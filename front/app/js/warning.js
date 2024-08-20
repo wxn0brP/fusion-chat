9f68117f8d812dda1c31d4a1317cf1a89e555e2c
wxn0brP <toja.filiper@gmail.com>
2024-08-20 17:40:09 +0200

    fix: save warning

--- front/app/js/warning.js
+++ front/app/js/warning.js
@@ -1,7 +1,14 @@
 (function(){
     if(debugFunc.isDebug) return;
-    lo("%cZaczekaj!", "font-size:60px;color:gold;");
-    lo("%cJeśli ktoś powiedział Ci, żebyś coś skopiował i wkleił tutaj, to istnieje 110% szans na to, że próbuje Cię oszukać.", "font-size:20px");
-    lo("%cWklejenie tu czegokolwiek może dać komuś dostęp do Twojego konta Fusion Chat i twoich wiadomości.", "font-size:20px;color:red");
-    lo("%cDopóki nie rozumiesz dokładnie tego, co robisz, zamknij to okno i zachowaj bezpieczeństwo.", "font-size:20px");
+
+    const warnings = [
+        { txt: "Wait!", css: "60px;color:gold" },
+        { txt: "If someone told you to copy and paste something here, there's a 110% chance they're trying to scam you.", css: "20px" },
+        { txt: "Pasting anything here could give someone access to your Fusion Chat account and your messages.", css: "20px;color:red" },
+        { txt: "Unless you fully understand what you're doing, close this window and stay safe.", css: "20px" }
+    ];
+
+    warnings.forEach(({ txt, css }) => {
+        lo(`%c${translateFunc.get(txt)}`, `font-size:${css}`);
+    });
 })();
