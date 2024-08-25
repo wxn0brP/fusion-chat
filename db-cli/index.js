595b2ff4bf31dec34d301d5c70e0e06cd00e6cf2
wxn0brP <toja.filiper@gmail.com>
2024-08-25 13:41:52 +0200

    Refactor | db cli

--- db-cli/index.js
+++ db-cli/index.js
@@ -1,14 +1,14 @@
-const { Command } = require('commander');
-const chalk = require('chalk');
-const fs = require('fs');
-const path = require('path');
-const mainMenu = require('./mainMenu');
+const { Command } = require("commander");
+const chalk = require("chalk");
+const fs = require("fs");
+const path = require("path");
+const mainMenu = require("./main");
 
-const configPath = path.join(__dirname, '.dbs.json');
+const configPath = path.join(__dirname, ".dbs.json");
 if(!fs.existsSync(configPath)){
-    fs.writeFileSync(configPath, '{}', 'utf8');
+    fs.writeFileSync(configPath, "{}", "utf8");
 }
-const extPath = path.join(__dirname, 'ext');
+const extPath = path.join(__dirname, "ext");
 if(!fs.existsSync(extPath)){
     fs.mkdirSync(extPath);
 }
@@ -19,67 +19,67 @@ global.lo = console.log;
 const program = new Command();
 
 function saveConfig(){
-    fs.writeFileSync(configPath, JSON.stringify(dbConfig, null, 4), 'utf8');
+    fs.writeFileSync(configPath, JSON.stringify(dbConfig, null, 4), "utf8");
 }
 
 program
-    .command('list')
-    .description('List all databases')
+    .command("list")
+    .description("List all databases")
     .action(() => {
         const dbs = Object.keys(dbConfig);
         if(dbs.length === 0){
-            console.log(chalk.yellow('No databases configured.'));
+            console.log(chalk.yellow("No databases configured."));
         }else{
-            console.log(chalk.green('Configured databases:'), dbs.join(', '));
+            console.log(chalk.green("Configured databases:"), dbs.join(", "));
         }
     });
 
 program
-    .command('add <name> <folder>')
-    .description('Add a new database')
+    .command("add <name> <folder>")
+    .description("Add a new database")
     .action((name, folder) => {
         if(dbConfig[name]){
-            console.log(chalk.red('Database with this name already exists.'));
+            console.log(chalk.red("Database with this name already exists."));
         }else{
             dbConfig[name] = folder;
             saveConfig();
-            console.log(chalk.green('Database added:'), name);
+            console.log(chalk.green("Database added:"), name);
         }
     });
 
 program
-    .command('rm <name>')
-    .description('Remove a database')
+    .command("rm <name>")
+    .description("Remove a database")
     .action((name) => {
         if(dbConfig[name]){
             delete dbConfig[name];
             saveConfig();
-            console.log(chalk.green('Database removed:'), name);
+            console.log(chalk.green("Database removed:"), name);
         }else{
-            console.log(chalk.red('Database not found.'));
+            console.log(chalk.red("Database not found."));
         }
     });
 
 program
-    .command('autoAdd <folder>')
-    .description('Automatically add all databases from <folder>')
+    .command("autoAdd <folder>")
+    .description("Automatically add all databases from <folder>")
     .action((folder) => {
         const dbs = fs.readdirSync(folder, { withFileTypes: true }).filter(dirent => dirent.isDirectory()).map(dirent => dirent.name);
         for(const db of dbs){
             if(dbConfig[db]) continue;
             dbConfig[db] = folder + "/" + db;
-            console.log(chalk.green('Database added:'), db);
+            console.log(chalk.green("Database added:"), db);
         }
         saveConfig();
     });
 
 program
-    .command('use')
-    .description('Work with a selected database')
+    .command("use")
+    .description("Work with a selected database")
     .action(async () => {
         const dbNames = Object.keys(dbConfig);
         if(dbNames.length === 0){
-            console.log(chalk.red('No databases configured. Please add a database first.'));
+            console.log(chalk.red("No databases configured. Please add a database first."));
             return;
         }
 
