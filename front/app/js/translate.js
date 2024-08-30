dc930f602a11a48b917a8298c06063d2e578d9e4
wxn0brP <toja.filiper@gmail.com>
2024-08-30 20:04:45 +0200

    fix: translate

--- front/app/js/translate.js
+++ front/app/js/translate.js
@@ -1,9 +1,13 @@
 const translateFunc = {
     locale: {},
+    categories: [
+        "html-ui", "js-ui", "socket", "other",
+    ],
 
     init(){
         document.querySelectorAll("[translate]").forEach(ele => ele.setAttribute("translate", ele.innerHTML));
         this.localesList = cw.get("lang/list.txt").split("\n");
+        this.localesList.unshift("en");
 
         let lang = localStorage.getItem("lang");
         if(!lang){
@@ -18,27 +22,34 @@ const translateFunc = {
     load(lang="en"){
         if(this.localesList.indexOf(lang) == -1) return;
         localStorage.setItem("lang", lang);
+        this.locale = {};
 
         if(lang == "en"){
-            this.locale = {};
             this.translateHTML();
             return;
         }
 
-        const data = cw.get("lang/"+lang+".jsonc");
-        this.locale = utils.parseJSONC(data);
+        this.categories.forEach(cat => {
+            const data = cw.get(`lang/${lang}/${cat}.jsonc`);
+            this.locale[cat] = utils.parseJSONC(data);
+        });
+
         this.translateHTML();
     },
 
     translateHTML(){
         document.querySelectorAll("[translate]").forEach(ele => {
             const text = ele.getAttribute("translate");
-            ele.innerHTML = this.locale[text] || text;
+            ele.innerHTML = translateFunc.get(text);
         });
     },
 
     get(text, ...data){
-        text = this.locale[text] || text;
+        for(const key of Object.keys(this.locale)){
+            const cat = this.locale[key];
+            if(cat[text]) text = cat[text];
+        }
+
         return text.replace(/\$/g, () => data.shift() || '$') 
     },
 }
