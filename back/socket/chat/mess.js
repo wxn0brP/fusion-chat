9f7c57d3a47b75949949f9f56d4677a5b2aa990f
wxn0brP <toja.filiper@gmail.com>
2024-09-15 19:20:18 +0200

    refactor: send message logic split into separate module

--- back/socket/chat/mess.js
+++ back/socket/chat/mess.js
@@ -1,3 +1,4 @@
+const sendMessage = require("../../logic/sendMessage");
 const chatMgmt = require("../../logic/chatMgmt");
 const valid = require("../../logic/validData");
 const permissionSystem = require("../../logic/permission-system");
@@ -8,117 +9,10 @@ const validShema = {
 };
 
 module.exports = (socket) => {
-    socket.ontimeout("mess", 200,
-    /**
-     * Handles the 'mess' event for sending messages in a chat.
-     * Validates the message request, checks if the chat exists, and sends the message to the recipient.
-     * Supports both friend chats and channel chats.
-     *
-     * @param {object} req - The request object containing message details.
-     * @param {string} req.to - The identifier of the recipient or chat.
-     * @param {string} req.msg - The message content.
-     * @param {string} req.chnl - The channel identifier.
-     * @param {string} [req.enc] - Optional flag to indicate if the message is encrypted.
-     * @param {string} [req.res] - Optional identifier for the message being replied to.
-     * @param {boolean} [req.silent] - Optional flag to send the message silently without notification.
-     */
-    async (req) => {
+    socket.ontimeout("mess", 200, async (req) => {
         try{
-            if(!socket.user) return socket.emit("error", "not auth");
-            if(typeof req !== "object") return socket.emit("error.valid", "mess", "req");
-            let { to, msg, chnl } = req;
-    
-            if(!valid.id(to))                           return socket.emit("error.valid", "mess", "to");
-            if(!valid.idOrSpecyficStr(chnl, ["main"]))  return socket.emit("error.valid", "mess", "chnl");
-            if(!valid.str(msg, 0, 2000))                return socket.emit("error.valid", "mess", "msg");
-
-            //optional
-            if(req.enc && !valid.str(req.enc, 0, 30))   return socket.emit("error.valid", "mess", "enc");
-            if(req.res && !valid.id(req.res))           return socket.emit("error.valid", "mess", "res");
-            if(req.silent && !valid.bool(req.silent))   return socket.emit("error.valid", "mess", "silent");
-            
-            let privChat = to.startsWith("$");
-            if(privChat){
-                const priv = await global.db.userDatas.findOne(socket.user._id, { priv: to.replace("$", "") });
-                if(!priv) return socket.emit("error", "priv not found");
-                if(priv.blocked) return socket.emit("error", "blocked");
-
-                const toPriv = await global.db.userDatas.findOne(to.replace("$", ""), { priv: socket.user._id });
-                if(!toPriv) return socket.emit("error", "priv not found");
-                if(toPriv.blocked) return socket.emit("error", "blocked");
-
-                let p1 = socket.user._id;
-                let p2 = to.replace("$", "");
-                to = chatMgmt.combinateId(p1, p2);
-                global.db.mess.checkCollection(to);
-            }else{
-                if(!chatMgmt.chatExsists(to)) return socket.emit("error", "chat is not exists");
-            }
-
-            if(!privChat){
-                const perm = await getChnlPerm(socket.user._id, to, chnl); 
-                if(!perm.visable) return socket.emit("error", "channel is not exists");
-                if(!perm.text) return socket.emit("error", "not perm to write");
-            }
-    
-            let message = msg.trim();
-            if(msg.length > 500) return socket.emit("error", "msg is too long");
-            
-            let data = {
-                fr: socket.user._id,
-                msg: message,
-                chnl,
-            }
-            if(req.enc) data.enc = req.enc;
-            if(req.res) data.res = req.res;
-    
-            let _id = await global.db.mess.add(to, data);
-    
-            if(!privChat) data.to = to;
-            else data.to = "$"+socket.user._id;
-            
-            data._id = _id._id;
-            if(req.silent) data.silent = req.silent || false;
-            sendToSocket(socket.user._id, "mess", {
-                fr: socket.user._id,
-                msg: data.msg,
-                chnl,
-                to: "@",
-                toM: req.to,
-                _id: _id._id,
-                enc: data.enc || undefined,
-                res: data.res || undefined,
-            });
-    
-            if(!privChat){
-                data.toM = to;
-                let chat = await global.db.usersPerms.find(to, r => r.uid);
-                const server = (await global.db.groupSettings.findOne(to, { _id: "set"}));
-                const fromMsg = `${server.name} @${socket.user.name}`;
-
-                chat.forEach(async u => {
-                    u = u.uid;
-                    if(u == socket.user._id) return;
-
-                    const group = await global.db.userDatas.findOne(u, { group: data.to });
-                    if(group.muted && group.muted != -1){
-                        const muted = group.muted;
-                        if(muted == 0) return;
-                        if(muted > new Date().getTime()) return;
-                    }
-
-                    sendToSocket(u, "mess", data);
-                    if(!data.silent) global.fireBaseMessage.send(u, "New message from " + fromMsg, data.msg);
-                })
-            }else{
-                let toSend = req.to.replace("$","");
-
-                data.toM = socket.user._id;
-                sendToSocket(toSend, "mess", data);
-
-                const user = await global.db.data.findOne("user", { _id: socket.user._id });
-                if(!data.silent) global.fireBaseMessage.send(toSend, "New message from " + user.name, data.msg);
-            }
+            const result = await sendMessage(req, socket.user);
+            if(result.err) return socket.emit(...result.err);
         }catch(e){
             socket.logError(e);
         }
