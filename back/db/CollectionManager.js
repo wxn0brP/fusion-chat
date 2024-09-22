90273382e2dbde7039e98e109a4d29054cc8f926
wxn0brP <toja.filiper@gmail.com>
2024-09-22 14:36:03 +0200

    fix: database context

--- back/db/CollectionManager.js
+++ back/db/CollectionManager.js
@@ -23,13 +23,14 @@ class CollectionManager{
      * @async
      * @function
      * @param {function|Object} search - The query. It can be an object or a function.
+     * @param {Object} context - The context object (for functions).
      * @param {Object} options - The options for the search.
      * @param {number} options.max - The maximum number of entries to return. Default is -1, meaning no limit.
      * @param {boolean} options.reverse - Whether to reverse the order of returned entries. Default is false.
      * @returns {Promise<Array<Object>>} A Promise that resolves with the matching data.
      */
-    async find(search, options={}){
-        return await this.db.find(this.collection, search, options);
+    async find(search, context={}, options={}){
+        return await this.db.find(this.collection, search, context, options);
     }
 
     /**
@@ -38,10 +39,11 @@ class CollectionManager{
      * @async
      * @function
      * @param {function|Object} search - The query. It can be an object or a function.
+     * @param {Object} context - The context object (for functions).
      * @returns {Promise<Object|null>} A Promise that resolves with the first matching data entry.
      */
-    async findOne(search){
-        return await this.db.findOne(this.collection, search);
+    async findOne(search, context={}){
+        return await this.db.findOne(this.collection, search, context);
     }
 
     /**
@@ -51,10 +53,11 @@ class CollectionManager{
      * @function
      * @param {function|Object} search - The query. It can be an object or a function.
      * @param {function|Object} arg - Update arguments.
+     * @param {Object} context - The context object (for functions).
      * @returns {Promise<boolean>} A Promise that resolves when the data is updated.
      */
-    async update(search, arg){
-        return await this.db.update(this.collection, search, arg);
+    async update(search, arg, context={}){
+        return await this.db.update(this.collection, search, arg, context);
     }
 
     /**
@@ -64,10 +67,11 @@ class CollectionManager{
      * @function
      * @param {function|Object} search - The query. It can be an object or a function.
      * @param {function|Object} arg - The query.
+     * @param {Object} context - The context object (for functions).
      * @returns {Promise<boolean>} A Promise that resolves when the data entry is updated.
      */
-    async updateOne(search, arg){
-        return await this.db.updateOne(this.collection, search, arg);
+    async updateOne(search, arg, context={}){
+        return await this.db.updateOne(this.collection, search, arg, context);
     }
 
     /**
@@ -76,11 +80,12 @@ class CollectionManager{
      * @async
      * @function
      * @param {function|Object} search - The query. It can be an object or a function.
+     * @param {Object} context - The context object (for functions).
      * @returns {Promise<boolean>} A Promise that resolves when the data is removed.
      */
 
-    async remove(search){
-        return await this.db.remove(this.collection, search);
+    async remove(search, context={}){
+        return await this.db.remove(this.collection, search, context);
     }
 
     /**
@@ -89,10 +94,11 @@ class CollectionManager{
      * @async
      * @function
      * @param {function|Object} search - The query. It can be an object or a function.
+     * @param {Object} context - The context object (for functions).
      * @returns {Promise<boolean>} A Promise that resolves when the data entry is removed.
      */
-    async removeOne(search){
-        return await this.db.removeOne(this.collection, search);
+    async removeOne(search, context={}){
+        return await this.db.removeOne(this.collection, search, context);
     }
 
     /**
@@ -101,11 +107,12 @@ class CollectionManager{
      * @param {function|Object} search - The query. It can be an object or a function.
      * @param {function|Object} arg - The search criteria for the update.
      * @param {function|Object} add_arg - The arguments to be added to the new entry.
+     * @param {Object} context - The context object (for functions).
      * @param {boolean} id_gen - Whether to generate an ID for the entry. Default is true.
      * @return {Promise<boolean>} A Promise that resolves to `true` if the entry was updated, or `false` if it was added.
      */
-    async updateOneOrAdd(search, arg, add_arg={}, id_gen=true){
-        return await this.db.updateOneOrAdd(this.collection, search, arg, add_arg, id_gen);
+    async updateOneOrAdd(search, arg, add_arg={}, context={}, id_gen=true){
+        return await this.db.updateOneOrAdd(this.collection, search, arg, add_arg, context, id_gen);
     }
 }
 
