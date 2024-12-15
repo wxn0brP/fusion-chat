51939b245b52d3857d13af27af49bad11630e45a
wxn0brP <toja.filiper@gmail.com>
2024-12-15 11:54:45 +0100

    fix: correct access to user ID when disconnecting

--- back/socket/chat/evt.js
+++ back/socket/chat/evt.js
@@ -2,12 +2,12 @@ import { rm } from "fs";
 import { rmCache as statusMgmtRmCache } from "../../logic/status.js";
 
 export default (socket) => {
+    const uid = socket.user._id;
     socket.on("disconnect", () => {
-        const uid = socket.user._id;
-        const sockets = global.getSocket(socket.user._id);
+        const sockets = global.getSocket(uid);
         if(sockets.length > 0) return;
 
-        rm(`userFiles/${socket.user._id}`, { recursive: true, force: true }, (err) => {
+        rm(`userFiles/${uid}`, { recursive: true, force: true }, (err) => {
             if(err) console.log(err);
         });
         statusMgmtRmCache(uid);
