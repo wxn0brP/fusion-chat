f793bc6aa94d6e430b91ef4ca26754ac475b3901
wxn0brP <toja.filiper@gmail.com>
2024-12-26 05:23:57 +0100

    feat(backend): add ts declare

---
+++ back/dataBase.d.ts
@@ -0,0 +1,23 @@
+import type db from "@wxn0brp/db/database.d.ts";
+import type graph from "@wxn0brp/db/graph.d.ts";
+
+declare global {
+    var db: {
+        [name: string]: db,
+        [name: string]: graph,
+        data: db,
+        dataGraph: graph,
+        system: db,
+        logs: db,
+
+        mess: db,
+        userData: db,
+        botData: db,
+
+        realmConf: db,
+        realmRoles: db,
+        realmUser: db,
+        realmData: db,
+        realmDataGraph: graph
+    }
+}
