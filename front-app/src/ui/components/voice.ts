a7aaab11abc2c4bbe575247323c7eea8495c2ff7
wxn0brP <toja.filiper@gmail.com>
2025-01-18 23:12:58 +0100

    fix(frontend): voice component

--- front-app/src/ui/components/voice.ts
+++ front-app/src/ui/components/voice.ts
@@ -8,8 +8,9 @@ import apis from "../../api/apis";
 import uiFunc from "../helpers/uiFunc";
 import vars from "../../var/var";
 import LangPkg, { langFunc } from "../../utils/translate";
+import Id from "../../types/Id";
 
-interface voiceFuncVar{
+interface voiceFuncVar {
     local_stream: null | MediaStream;
     muteMic: boolean;
     sending: boolean | number;
@@ -24,38 +25,39 @@ const voiceFuncVar: voiceFuncVar = {
 }
 
 const voiceFunc = {
-    async initCall(){
-        try{
+    async initCall() {
+        try {
             voiceHTML.voiceShow.style.display = "";
-            if(this.local_stream) return;
+            if (this.local_stream) return;
             const stream = await this.getStream(true, false);
             voiceFuncVar.local_stream = stream;
             voiceHTML.div.fadeIn();
-        }catch(error){
+        } catch (error) {
             console.error('initCall', `Error joining voice channel: ${error.message}`);
         }
     },
 
-    async joinToVoiceChannel(to){
+    async joinToVoiceChannel(to: Id) {
         await this.initCall();
         this.joined = to;
         socket.emit("voice.join", to);
         socket.emit("voice.get.users");
+        voiceHTML.muteMic.innerHTML = voiceFuncVar.muteMic ? LangPkg.ui.mute.unmute : LangPkg.ui.mute.mute;
     },
 
-    send(){
-        if(this.sending) return;
+    send() {
+        if (this.sending) return;
         const _this = this;
 
         let buffer = [];
         const mediaRecorder = new MediaRecorder(this.local_stream, { mimeType: "video/webm; codecs=vp8,opus" });
         mediaRecorder.ondataavailable = (event) => {
-            if(event.data.size == 0) return;
+            if (event.data.size == 0) return;
             buffer.push(event.data);
         };
 
         mediaRecorder.onstop = () => {
-            if(buffer.length == 0) return lo("no voice data");
+            if (buffer.length == 0) return lo("no voice data");
 
             socket.volatile.emit("voice.sendData", buffer);
             buffer = [];
@@ -64,7 +66,7 @@ const voiceFunc = {
         this.sending = setInterval(() => {
             mediaRecorder.stop();
             setTimeout(() => {
-                if(!_this.sending) return;
+                if (!_this.sending) return;
                 mediaRecorder.start();
             }, 10);
         }, 100);
@@ -85,8 +87,8 @@ const voiceFunc = {
     //     return audio;
     // },
 
-    endCall(){
-        if(this.sending) clearInterval(this.sending);
+    endCall() {
+        if (this.sending) clearInterval(this.sending);
         this.sending = false;
         this.joined = false;
         socket.emit("voice.leave");
@@ -95,11 +97,11 @@ const voiceFunc = {
         voiceHTML.mediaContainer.innerHTML = "";
         voiceHTML.voiceShow.style.display = "none";
 
-        if(apis.app.apiType == "rn"){
+        if (apis.app.apiType == "rn") {
             apis.api.send({
                 type: "stopAudio",
             });
-        }else{
+        } else {
             voiceFuncVar.local_stream.getTracks().forEach((track) => {
                 track.stop();
             });
@@ -107,23 +109,23 @@ const voiceFunc = {
         }
     },
 
-    startCall(){
-        const id = vars.chat.to.replace("$","");
-        if(id == "main") return;
+    startCall() {
+        const id = vars.chat.to.replace("$", "");
+        if (id == "main") return;
 
         const isConfirm = confirm(langFunc(LangPkg.ui.confirm.call_to, apis.www.changeUserID(id)) + "?");
-        if(!isConfirm) return;
+        if (!isConfirm) return;
 
         socket.emit("call.dm.init", id);
     },
 
-    toggleMute(){
+    toggleMute() {
         voiceFuncVar.muteMic = !voiceFuncVar.muteMic;
-        if(apis.app.apiType == "rn"){
+        if (apis.app.apiType == "rn") {
             apis.api.send({
                 type: voiceFuncVar.muteMic ? "stopAudio" : "startAudio",
             })
-        }else{
+        } else {
             const tracks = voiceFuncVar.local_stream.getAudioTracks();
             tracks.forEach((track) => {
                 track.enabled = !voiceFuncVar.muteMic;
@@ -133,44 +135,44 @@ const voiceFunc = {
         voiceHTML.muteMic.innerHTML = voiceFuncVar.muteMic ? LangPkg.ui.mute.unmute : LangPkg.ui.mute.mute;
     },
 
-    async getStream(audio=true, video=false){
-        if(apis.app.apiType == "rn"){
+    async getStream(audio = true, video = false) {
+        if (apis.app.apiType == "rn") {
             // @ts-ignore
             // TODO fix type
             return await processMediaRN.getStream();
         }
         const stream = new MediaStream();
 
-        async function getUserMedia(options){
+        async function getUserMedia(options: { audio?: { deviceId?: string } | boolean, video?: { deviceId?: string } | boolean }) {
             // TODO fix type
-            if(navigator.mediaDevices?.getUserMedia){
+            if (navigator.mediaDevices?.getUserMedia) {
                 return await navigator.mediaDevices.getUserMedia(options);
                 // @ts-ignore
-            }else if(navigator.webkitGetUserMedia){
+            } else if (navigator.webkitGetUserMedia) {
                 // @ts-ignore
                 return await navigator.webkitGetUserMedia(options);
                 // @ts-ignore
-            }else if(navigator.mozGetUserMedia){
+            } else if (navigator.mozGetUserMedia) {
                 // @ts-ignore
                 return await navigator.mozGetUserMedia(options);
             }
         }
 
-        async function selectDevice(devices, prompt){
+        async function selectDevice(devices, prompt) {
             const labels = devices.map(device => device.label);
             const deviceIds = devices.map(device => device.deviceId);
             const selectedIndex = await uiFunc.selectPrompt(prompt, labels, deviceIds) as number;
             return deviceIds[selectedIndex];
         }
 
-        try{
-            const permisons = await getUserMedia({ audio, video });
-            if(!permisons){
+        try {
+            const permissions = await getUserMedia({ audio, video });
+            if (!permissions) {
                 uiFunc.uiMsgT('Error getting stream');
                 return stream;
             }
             setTimeout(() => {
-                permisons.getTracks().forEach(track => track.stop());
+                permissions.getTracks().forEach(track => track.stop());
             }, 100);
 
             const devices = await navigator.mediaDevices.enumerateDevices();
@@ -181,36 +183,36 @@ const voiceFunc = {
             const videoOptions = video ? { deviceId: await selectDevice(videoDevices, LangPkg.ui.call.select_video_device) } : false;
 
             const mediaStream = await getUserMedia({ audio: audioOptions, video: videoOptions });
-            if(mediaStream)
+            if (mediaStream)
                 stream.addTrack(mediaStream.getAudioTracks()[0]);
 
             return stream;
-        }catch(error){
+        } catch (error) {
             console.error(`Error getting stream: ${error.message}`);
             return stream;
         }
     },
 
-    isInUserCall(id){
+    isInUserCall(id: Id) {
         const room = "user_" + [id, vars.user._id].sort().join("=");
         return room == voiceFuncVar.joined;
     }
 }
 
-socket.on("voice.sendData", (from, data) => {
+socket.on("voice.sendData", (from: Id, data: any) => {
     const blob = new Blob(data, { type: "audio/webm; codecs=vp8,opus" });
     const url = URL.createObjectURL(blob);
     const audio = new Audio(url);
-    audio.play().catch(() => {});
+    audio.play().catch(() => { });
 });
 
 socket.on("connect", () => {
-    if(!voiceFuncVar.joined) return;
+    if (!voiceFuncVar.joined) return;
     lo("reconnected to voice channel");
-    voiceFunc.joinToVoiceChannel(voiceFuncVar.joined);
+    voiceFunc.joinToVoiceChannel(voiceFuncVar.joined as Id);
 });
 
-socket.on("voice.get.users", (users) => {
+socket.on("voice.get.users", (users: Id[]) => {
     voiceHTML.users.innerHTML = "";
     users.forEach((user) => {
         const li = document.createElement("li");
@@ -218,49 +220,49 @@ socket.on("voice.get.users", (users) => {
         voiceHTML.users.appendChild(li);
     });
 
-    if(users.length > 1){
+    if (users.length > 1) {
         voiceFunc.send();
-    }else if(users.length == 1){
+    } else if (users.length == 1) {
         clearInterval(voiceFuncVar.sending as number);
         voiceFuncVar.sending = false;
     }
 });
 
-socket.on("call.dm.init", (id, userOffline=false) => {
-    if(userOffline){
+socket.on("call.dm.init", (id: Id, userOffline: boolean = false) => {
+    if (userOffline) {
         uiFunc.uiMsgT(LangPkg.ui.call.offline, apis.www.changeUserID(id));
         const join = confirm(LangPkg.ui.call.wait + "?");
-        if(!join) return;
-    }else{ // if user is online
-        if(voiceFunc.isInUserCall(id))
+        if (!join) return;
+    } else { // if user is online
+        if (voiceFunc.isInUserCall(id))
             return socket.emit("call.dm.answer", id, true);
-        
+
         const isConfirm = confirm(langFunc(LangPkg.ui.call.called, apis.www.changeUserID(id)) + "?");
         socket.emit("call.dm.answer", id, isConfirm);
-    
-        if(!isConfirm) return;
+
+        if (!isConfirm) return;
     }
 
     const room = "user_" + [id, vars.user._id].sort().join("=");
     voiceFunc.joinToVoiceChannel(room);
 });
 
-socket.on("call.dm.answer", (id, answer) => {
-    if(!answer)
+socket.on("call.dm.answer", (id: Id, answer: boolean) => {
+    if (!answer)
         return alert(LangPkg.ui.call.rejected);
 
     const isConfirm = confirm(langFunc(LangPkg.ui.call.answer, apis.www.changeUserID(id)) + "?");
-    if(!isConfirm) return;
+    if (!isConfirm) return;
 
     const room = "user_" + [id, vars.user._id].sort().join("=");
     voiceFunc.joinToVoiceChannel(room);
 });
 
-socket.on("voice.leave", (id) => {
+socket.on("voice.leave", (id: Id) => {
     uiFunc.uiMsgT(LangPkg.ui.call.left, apis.www.changeUserID(id));
 });
 
-socket.on("voice.join", (to) => {
+socket.on("voice.join", (to: Id) => {
     uiFunc.uiMsgT(LangPkg.ui.call.joined, apis.www.changeUserID(to));
 });
 
