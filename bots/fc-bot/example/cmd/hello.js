43df749f57e39bfaff7a840d07efb49a5f4a2619
wxn0brP <toja.filiper@gmail.com>
2025-01-30 21:32:21 +0100

    clean: remove migrated dir

--- bots/fc-bot/example/cmd/hello.js
+++
@@ -1,8 +0,0 @@
-export const name = "hello";
-export const alias = ["hi"];
-export const minArgs = 1;
-export const usage = "<name>";
-export async function execute(mess, args){
-    const [name] = args;
-    mess.reply(`Hello, ${name}!`);
-}
