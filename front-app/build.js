e7e6a104f4488af226454050204efca0eb998e1c
wxn0brP <toja.filiper@gmail.com>
2025-02-22 14:27:57 +0100

    feat(frontend): upgrade the build script

--- front-app/build.js
+++ front-app/build.js
@@ -1,4 +1,19 @@
+#!/usr/bin/env node
+/**
+ * Example of config usage:
+ * node build.js minify=false
+ */
 import esbuild from "esbuild";
+
+const args = process.argv.slice(2);
+const config = {};
+for (const arg of args) {
+    let [key, value] = arg.split("=");
+    if(value == undefined) value = true;
+    else if (value == "true" || value == "false") value = value == "true";
+    else if (value.startsWith("{") && value.endsWith("}")) value = JSON.parse(value);
+    config[key] = value;
+}
 
 esbuild.build({
     entryPoints: ["src/startApp.ts"],
@@ -8,7 +23,7 @@ esbuild.build({
     platform: "browser",
     target: "es2022",
     sourcemap: true,
-    minify: false,
+    minify: true,
     treeShaking: true,
     splitting: false,
     keepNames: true,
@@ -17,5 +32,6 @@ esbuild.build({
     logLevel: "info",
     external: [
         "../../dist/lang/*"
-    ]
+    ],
+    ...config
 }).catch(() => process.exit(1));
