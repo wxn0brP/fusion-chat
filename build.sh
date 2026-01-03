b51beb213d0186fc415443b20f18f5b755e52659
wxn0brP <toja.filiper@gmail.com>
2026-01-03 01:41:56 +0100

    fix: build

--- build.sh
+++ build.sh
@@ -21,4 +21,4 @@ bun build back/app.ts \
   --external source-map-support \
   --external '#runtime/*'
 
-tsc ./back/config-base/**/*.ts --outDir ./dist-back/config-base
+tsc ./back/config-base/*.ts --outDir ./dist-back/config-base
